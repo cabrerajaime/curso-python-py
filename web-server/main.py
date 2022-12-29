07095dc14c1b4e3460b9ee42a54c8f96e6550bb1
Jaime Cabrera <cabrerajaime877@gmail.com>
2022-12-28 21:20:01 -0600

    Web Server con FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
 
+@app.get('/')
+def get_list():
+    return [1,2,3]
 
+@app.get('/contact',response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>Algo de HTML</title>
+        </head>
+        <body>
+            <h1>Soy una Página de HTML!</h1>
+            <p> Mi nombre es Jaime </p>
+        </body>
+    </html>
+    """
 
 def run():
     store.get_categories()
